8d75e1e940445b3fe6713f462210fc50d95939e5
Jason Parsons <jason@jspars.com>
2024-02-19 07:37:08 -0500

    tidy

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
     }),
 
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/ml-psych' : '',
+      base: process.env.NODE_ENV === "production" ? "/ml-psych" : "",
     },
   }
 };
