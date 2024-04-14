1ca8488f6fa869d3d4f6b01d087efd56dfca340f
Jason Parsons <jason@jspars.com>
2024-04-14 15:52:50 -0400

    max width container added on fees page

--- src/routes/fees_and_insurance/+page.svelte
+++ src/routes/fees_and_insurance/+page.svelte
@@ -9,7 +9,8 @@
 
 <PageHeader headerText="Fees and Insurance" backgroundImage="/images/fees-header.png"/>
 
-<div class="my-5 mx-auto container">
+<div class="my-5 container max-w-[1000px]">
+	
 	<p class="">
 		Below is a summary of my fees, and it is broken into rates for individuals and rates for couples.
 		Each group lists the price for the requried intake session and each session thereafter.
@@ -17,20 +18,22 @@
 		them when submitting the <a href="{base}/contact">Contact Form</a>.
 	</p>
 	<Prices />
-</div>
-<div class="p-10 mt-10">
-	<h3>
-		Insurances Accepted<span class="font-light text-sm align-top text-slate-600">†*</span>
-	</h3>
-	<div class="">
-		<h4 class="font-normal text-slate-600">Anthem/BCBS</h4>
-		<h4 class="font-normal text-slate-600">Aetna</h4>
-		<p class="text-sm text-slate-500">
-			† Couples therapy is not covered by most health insurance policies. See FAQ for more
-			information.<br />
-			* Currently accepting these insurances for Virginia residents only.
-		</p>
+	
+	<div class="p-10 mt-10">
+		<h3>
+			Insurances Accepted<span class="font-light text-sm align-top text-slate-600">†*</span>
+		</h3>
+		<div class="">
+			<h4 class="font-normal text-slate-600">Anthem/BCBS</h4>
+			<h4 class="font-normal text-slate-600">Aetna</h4>
+			<p class="text-sm text-slate-500">
+				† Couples therapy is not covered by most health insurance policies. See FAQ for more
+				information.<br />
+				* Currently accepting these insurances for Virginia residents only.
+			</p>
+		</div>
 	</div>
+
 </div>
 
 <style lang="scss">
