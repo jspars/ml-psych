430e76a1bd4a2ecee8dcf00f5050033ef0322923
Jason Parsons <jason@jspars.com>
2024-04-14 15:46:09 -0400

    accordian collapse bug in prod

--- src/lib/components/Faq.svelte
+++ src/lib/components/Faq.svelte
@@ -12,7 +12,7 @@
 					type="button"
 					data-bs-toggle="collapse"
 					data-bs-target="#collapseOne"
-					aria-expanded="true"
+					aria-expanded="false"
 					aria-controls="collapseOne"
 				>
 					How do we get started?
@@ -20,7 +20,7 @@
 			</h2>
 			<div
 				id="collapseOne"
-				class="accordion-collapse collapse show"
+				class="accordion-collapse collapse"
 				data-bs-parent="#accordion"
 			>
 				<div class="accordion-body">
@@ -161,79 +161,11 @@
 			</div>
 		</div>
 	</div>
-
-	
-	<!-- <div class="prose mx-auto mb-5">
-		<h2 class="text-xl mb-3 mx-auto">How do we get started?</h2>
-		<p class="mx-auto">
-			Complete the contact form. Once received, I will reach out to schedule an initial consultation
-			if I am currently accepting new clients.
-		</p>
-		<p class="mx-auto">
-			The initial consultation is a 20 to 30 minute, virtual, no cost, consultation to get know you,
-			what is bringing you to therapy, and to assess whether we are a good fit to work together. I
-			will ask you some questions, tell you more about my therapeutic approach, and you will have
-			time to ask me questions as well.
-		</p>
-		<p class="mx-auto">
-			The working relationship between therapist and client is vital to the success of therapy, and
-			therapy is a significant investment of time and resources, so finding a therapist with whom
-			you feel a good rapport is important.
-		</p>
-	</div> -->
-
-	<!-- <div class="prose mx-auto mb-5">
-		<h2 class="text-xl mb-3 mx-auto">Do you accept insurance?</h2>
-		<p class="mx-auto">
-			I currently work with a third party platform to bill insurance for 
-			<span class="font-bold">individual therapy</span>. If we agree to 
-			work together, and you wish to use insurance, we will discuss this platform
-			and billing during our initial consultation. I currently can accept 
-			Anthem/BCBS and Aetna for Virginia residents only. For more information, 
-			see the	<a href="{base}/fees_and_insurance">fees and insurance</a>
-			page.
-		</p>
-	</div> -->
-
-	<!-- <div class="prose mx-auto mb-5">
-		<h2 class="text-xl mb-3 mx-auto">Can I use insurance for couples or relationship therapy?</h2>
-		<p class="mx-auto">
-			Unfortunately, probably not. Many health insurance policies do not cover relationship,
-			couples, or marital therapy, as couples therapy is not specifically focused on treating a
-			mental health disorder. Couples therapy is focused on helping individuals improve their
-			romantic relationships. This is not considered a medically necessary service by most health
-			insurance policies.
-		</p>
-	</div> -->
-
-	<!-- <div class="prose mx-auto mb-5">
-		<h2 class="text-xl mb-3 mx-auto">What is an intake?</h2>
-		<p class="mx-auto">
-			An intake is a first session, where I will gather background information related to your
-			reasons for seeking therapy, your developmental history, and other information that helps me
-			better understand you and what is bringing you to therapy. Ideally, I prefer 90 minutes for
-			the intake, to allow enough time to gather this information.
-		</p>
-	</div> -->
-
-	<!-- <div class="prose mx-auto mb-5">
-		<h2 class="text-xl mb-3 mx-auto">How long do sessions last?</h2>
-		<p class="mx-auto">
-			Individual sessions typically last between 50 and 55 minutes and are held weekly. We may meet
-			less or more frequently if appropriate. We will discuss frequency of sessions in our first
-			session.
-		</p>
-
-		<p class="mx-auto">
-			Couples sessions vary in length due to various factors. When possible, I prefer to see couples
-			for at least 75 minutes, once a week. If schedules do not allow for a 75 minute session, we
-			will meet for 50-55 minutes.
-		</p>
-	</div> -->
 </div>
 
 <style>
-.collapse.show {
-  visibility: visible;
-}
+	.collapse {
+		visibility: visible;
+	}
+
 </style>
