9fac8d8e0bc9276f18c9fbdf49fde5acfdc890de
Jason Parsons <jason@jspars.com>
2026-01-11 15:04:11 -0500

    2nd part of diff fix

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,7 +9,7 @@
             <div class="col-6 small">
                 <div class="lc-block">
                     <div editable="rich">
-                        <p>© Lawson Psychotherapy 2025</p>
+                        <p>© Lawson Psychotherapy 2026</p>
                     </div>
                 </div>
                 <!-- /lc-block -->
