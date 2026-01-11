e8b66d6cfdbd529444440c4c8c1d320036079681
Jason Parsons <jason@jspars.com>
2026-01-11 15:00:48 -0500

    Revert "updated language"
    This reverts commit 6269144af596f12ad19ddd4acc3d657cfca71007.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-    server: {
-        https: true, // Enable HTTPS in the server options
-        proxy: {},
-    },
-	plugins: [sveltekit(), mkcert()]
+	plugins: [sveltekit()]
 });
